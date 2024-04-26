d763676fc09f5385a1ae7b6b17f4810751ac10c4
Solomon-77 <cyrilleustaquio111222@gmail.com>
2024-04-26 19:39:12 +0800

    update

--- lib/bottom_nav/browse.dart
+++ lib/bottom_nav/browse.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Browse extends StatelessWidget {
@@ -5,8 +6,122 @@ class Browse extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Browse')),
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                "Fashion Styles",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: "Montserrat"),
+              ),
+              const SizedBox(height: 20),
+              const CupertinoSearchTextField(),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView( // Wrap the cards with a ListView
+                  children: [
+                    Card(
+                      elevation: 3,
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10)
+                            ),
+                            child: Image.asset(
+                              "images/bohemian.jpg",
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                              "Bohemian Style",
+                              style: TextStyle(
+                                fontFamily: "Montserrat",
+                                fontSize: 17
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Card(
+                      elevation: 3,
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10)
+                            ),
+                            child: Image.asset(
+                              "images/bohemian.jpg",
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                              "Casual Style",
+                              style: TextStyle(
+                                fontFamily: "Montserrat",
+                                fontSize: 17
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Card(
+                      elevation: 3,
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10)
+                            ),
+                            child: Image.asset(
+                              "images/bohemian.jpg",
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                              "Bohemian Style",
+                              style: TextStyle(
+                                fontFamily: "Montserrat",
+                                fontSize: 17
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
